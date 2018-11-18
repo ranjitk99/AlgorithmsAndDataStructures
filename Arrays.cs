570052e86b842658ffffeeb0180386f423a10e8c
Ranjitk99 <ranjitk9@outlook.com>
2018-11-17 20:05:47 -0800

    added new question find duplicates in an array

--- Arrays.cs
+++ Arrays.cs
@@ -117,3 +117,36 @@ public class Solution {
         return -1;
     }
 }
+
+
+///Given an array that contains numbers from 1 to n-1 and exactly 1 duplicate, find that duplicate.
+
+class GFG 
+{ 
+    static void printRepeating(int []arr, 
+                            int size) 
+    { 
+        int i;  
+          
+        Console.Write("The repeating" +  
+                       " elements are : "); 
+      
+        for (i = 0; i < size; i++) 
+        { 
+            if (arr[Math.Abs(arr[i])] >= 0) 
+                arr[Math.Abs(arr[i])] = 
+                    -arr[Math.Abs(arr[i])]; 
+            else
+                Console.Write(Math.Abs(arr[i]) + " "); 
+        }          
+    }  
+  
+    // Driver program  
+    public static void Main()  
+    { 
+        int []arr = {1, 2, 3, 1, 3, 6, 6}; 
+        int arr_size = arr.Length; 
+  
+        printRepeating(arr, arr_size); 
+    } 
+} 
