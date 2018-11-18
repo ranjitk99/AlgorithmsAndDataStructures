cba1ec025936a79bff98fbd1bed5a7916e396960
Ranjitk99 <ranjitk9@outlook.com>
2018-11-17 20:23:57 -0800

    #Added two new questions on arrays

--- Arrays.cs
+++ Arrays.cs
@@ -150,3 +150,123 @@ class GFG
         printRepeating(arr, arr_size); 
     } 
 } 
+
+////5. Search an element in an array where difference between adjacent elements is 1.
+///For example: arr[] = {8, 7, 6, 7, 6, 5, 4, 3, 2, 3, 4, 3}
+//Search for 3 and Output: Found at index 7
+public class GFG  
+{ 
+      
+    // in arr[0..n-1] 
+    static int search(int []arr, int n, 
+                      int x) 
+    { 
+          
+        // Travers the given array starting  
+        // from leftmost element 
+        int i = 0; 
+        while (i < n) 
+        { 
+              
+            // If x is found at index i 
+            if (arr[i] == x) 
+                return i; 
+      
+            // Jump the difference between  
+            // current array element and x 
+            i = i + Math.Abs(arr[i] - x); 
+        } 
+      
+        Console.WriteLine ("number is not" + 
+                           " present!"); 
+  
+        return -1; 
+    } 
+  
+    // Driver code 
+    public static void Main()  
+    { 
+          
+        int []arr = {8 ,7, 6, 7, 6, 5, 
+                     4,3, 2, 3, 4, 3 }; 
+        int n = arr.Length; 
+        int x = 3; 
+        Console.WriteLine("Element " + x +  
+                        " is present at index "
+                        + search(arr, n, 3)); 
+    } 
+} 
+
+//Given an array of numbers, split the array into two where one array contains the sum of n-1 numbers 
+//and the other array with all the n-1 elements
+//// C# program to split an array  
+// into Two equal sum subarrays 
+//Time Complexity : O(n)
+//Auxiliary Space : O(1)
+
+class GFG { 
+   
+    // Returns split point. If not possible, then 
+    // return -1. 
+    static int findSplitPoint(int []arr, int n) 
+    { 
+       
+    // traverse array element and compute sum 
+    // of whole array 
+    int leftSum = 0; 
+       
+    for (int i = 0 ; i < n ; i++) 
+        leftSum += arr[i]; 
+   
+    // again traverse array and compute right  
+    // sum and also check left_sum equal to  
+    // right sum or not 
+    int rightSum = 0; 
+       
+    for (int i = n-1; i >= 0; i--) 
+    { 
+        // add current element to right_sum 
+        rightSum += arr[i]; 
+   
+        // exclude current element to the left_sum 
+        leftSum -= arr[i] ; 
+   
+        if (rightSum == leftSum) 
+            return i ; 
+    } 
+   
+    // if it is not possible to split array 
+    // into two parts. 
+    return -1; 
+    } 
+   
+    // Prints two parts after finding split  
+    // point using findSplitPoint() 
+    static void printTwoParts(int []arr, int n) 
+    { 
+        int splitPoint = findSplitPoint(arr, n); 
+       
+        if (splitPoint == -1 || splitPoint == n ) 
+        { 
+            Console.Write("Not Possible" ); 
+            return; 
+        } 
+        for (int i = 0; i < n; i++) 
+        { 
+            if(splitPoint == i) 
+                 Console.WriteLine(); 
+                   
+             Console.Write(arr[i] + " "); 
+        } 
+    } 
+   
+    // Driver program 
+    public static void Main (String[] args) { 
+       
+    int []arr = {1 , 2 , 3 , 4 , 5 , 5 }; 
+    int n = arr.Length; 
+       
+    printTwoParts(arr, n); 
+           
+    } 
+} 
