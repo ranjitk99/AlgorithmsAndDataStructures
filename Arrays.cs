aa80b2e7ad73a26e3285c281ea0186ee028252d2
ranjitk99 <45135508+ranjitk99@users.noreply.github.com>
2018-11-17 19:38:56 -0800

    Update Arrays.cs

--- Arrays.cs
+++ Arrays.cs
@@ -8,8 +8,7 @@ class GFG {
     public static void search(int[] arr,  
                               int low,  
                               int high) 
-    { 
-          
+    {           
         if(low > high) 
             return; 
         if(low == high) 
@@ -53,85 +52,68 @@ class GFG {
 } 
 
 //Given a sorted array of n integers that has been rotated an unknown number of times, write code to find an element in the array. You may assume that the array was originally sorted in increasing order
-class main { 
-  
-    // Searches an element key in a 
-    // pivoted sorted array arrp[] 
-    // of size n 
-    static int pivotedBinarySearch(int[] arr, 
-                                int n, int key) 
-    { 
-        int pivot = findPivot(arr, 0, n - 1); 
-  
-        // If we didn't find a pivot, then 
-        // array is not rotated at all 
-        if (pivot == -1) 
-            return binarySearch(arr, 0, n - 1, key); 
-  
-        // If we found a pivot, then first 
-        // compare with pivot and then 
-        // search in two subarrays around pivot 
-        if (arr[pivot] == key) 
-            return pivot; 
-  
-        if (arr[0] <= key) 
-            return binarySearch(arr, 0, pivot - 1, key); 
-  
-        return binarySearch(arr, pivot + 1, n - 1, key); 
-    } 
-  
-    /* Function to get pivot. For array  
-    3, 4, 5, 6, 1, 2 it returns 
-    3 (index of 6) */
-    static int findPivot(int[] arr, int low, int high) 
-    { 
-        // base cases 
-        if (high < low) 
-            return -1; 
-        if (high == low) 
-            return low; 
-  
-        /* low + (high - low)/2; */
-        int mid = (low + high) / 2; 
-  
-        if (mid < high && arr[mid] > arr[mid + 1]) 
-            return mid; 
-  
-        if (mid > low && arr[mid] < arr[mid - 1]) 
-            return (mid - 1); 
-  
-        if (arr[low] >= arr[mid]) 
-            return findPivot(arr, low, mid - 1); 
-  
-        return findPivot(arr, mid + 1, high); 
-    } 
-  
-    /* Standard Binary Search function */
-    static int binarySearch(int[] arr, int low, 
-                            int high, int key) 
-    { 
-        if (high < low) 
-            return -1; 
-  
-        /* low + (high - low)/2; */
-        int mid = (low + high) / 2; 
-  
-        if (key == arr[mid]) 
-            return mid; 
-        if (key > arr[mid]) 
-            return binarySearch(arr, (mid + 1), high, key); 
-  
-        return binarySearch(arr, low, (mid - 1), key); 
-    } 
-  
-    // Driver Code 
-    public static void Main() 
-    { 
-        // Let us search 3 in below array 
-        int[] arr1 = { 5, 6, 7, 8, 9, 10, 1, 2, 3 }; 
-        int n = arr1.Length; 
-        int key = 3; 
-        Console.Write("Index of the element is : "
-                    + pivotedBinarySearch(arr1, n, key)); 
-    } 
-} 
+//Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+//(i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+//You are given a target value to search. If found in the array return its index, otherwise return -1.
+//You may assume no duplicate exists in the array.
+//Your algorithm's runtime complexity must be in the order of O(log n).
+
+public class Solution {
+    public int Search(int[] nums, int target) {
+         if (null == nums || nums.Length == 0)
+        {
+            return -1;
+        }
+
+        //Entry to recursion.
+        return BinarySearchInRotatedArray(0, nums.Length - 1, nums, target);
+    }
+    
+       private static int BinarySearchInRotatedArray(int low, int high, int[] nums, int target)
+    {
+        //Base case for NOT FOUND. 
+        if (low > high)
+        {
+            return -1;
+        }
+
+        int mid = low + ((high - low) / 2);
+
+        //Base case for FOUND. 
+        if (nums[mid] == target)
+        {
+            return mid;
+        }
+
+        //IMPORTANT: When pivoting at mid, one side (including mid) will be in sorted order.
+        //So we apply the process of elimination by: 
+        //finding which side is sorted and if the target is present on the sorted side.
+
+        //Entry here means left to mid is sorted.
+        if (nums[low] <= nums[mid])
+        {               
+            if (target >= nums[low] && target < nums[mid])
+            {
+                return BinarySearchInRotatedArray(low, mid - 1, nums, target);
+            }
+            else
+            {
+                return BinarySearchInRotatedArray(mid + 1, high, nums, target);
+            }
+        }
+        //Entry here means right to mid is sorted.
+        if (nums[mid] <= nums[high])
+        {               
+            if (target > nums[mid] && target <= nums[high])
+            {
+                return BinarySearchInRotatedArray(mid + 1, high, nums, target);
+            }
+            else
+            {
+                return BinarySearchInRotatedArray(low, mid - 1, nums, target);
+            }
+        }
+
+        return -1;
+    }
+}
